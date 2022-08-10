e64b8cdc85b442df7b48bdc84e5165ea5c4d9ebe
CedricD2268 <cedricdouillard08@outlook.com>
2022-08-10 15:41:23 -0400

    init

--- client/src/Reducers/MediaLoginErrorReducer.js
+++
@@ -1,9 +0,0 @@
-const MediaLoginErrorReducer = (state = '', action) => {
-    switch (action.type){
-        case 'MEDIA-ERROR':
-            return action.payload;
-        default:
-            return state;
-    }
-}
-export default MediaLoginErrorReducer;
--- server/middleware/upload.js
+++
@@ -1,8 +0,0 @@
-const multer = require('multer');
-
-const upload = multer({
-  storage: multer.memoryStorage(),
-  // file size limitation in bytes
-  limits: { fileSize: 52428800 },
-});
-
