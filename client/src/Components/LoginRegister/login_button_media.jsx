f1425bb6926956ea0364c8676518cc25d1e3b894
CedricD2268 <cedricdouillard08@outlook.com>
2022-08-18 19:21:14 -0400

    testing 1.0!

--- client/src/Components/LoginRegister/login_button_media.jsx
+++ client/src/Components/LoginRegister/login_button_media.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoginRegisterStyle from '../../Assets/scss/Login_Register/login_register.module.css'
-import { GoogleLogin } from 'react-google-login';
+// import { GoogleLogin } from 'react-google-login';
 import { useDispatch} from 'react-redux';
 import {LoginRx} from "../../Actions";
 import {MediaLoginErrorRx} from "../../Actions";
@@ -16,47 +16,47 @@ const LoginButtonMedia = ({google_t}) => {
     //     console.log(googleData.tokenId);
     // };
 
-    const handleLogin = async (googleData) => {
-        const bodyData = {"token": googleData.tokenId}
-        try {
-            const response = await fetch('http://localhost:5000/auth/google/verify', {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                credentials: 'include',
-                body: JSON.stringify(bodyData)
-            });
-            const parseRes = await response.json();
-            console.log(parseRes)
-            if (parseRes.token) {
-                dispatch(LoginRx());
-            }else{
-                dispatch(MediaLoginErrorRx(parseRes));
-                console.log(parseRes)
-            }
-
-        } catch (err) {
-            console.error(err.message);
-        }
-        return false;
-    }
+    // const handleLogin = async (googleData) => {
+    //     const bodyData = {"token": googleData.tokenId}
+    //     try {
+    //         const response = await fetch('http://localhost:5000/auth/google/verify', {
+    //             method: "POST",
+    //             headers: {"Content-Type": "application/json"},
+    //             credentials: 'include',
+    //             body: JSON.stringify(bodyData)
+    //         });
+    //         const parseRes = await response.json();
+    //         console.log(parseRes)
+    //         if (parseRes.token) {
+    //             dispatch(LoginRx());
+    //         }else{
+    //             dispatch(MediaLoginErrorRx(parseRes));
+    //             console.log(parseRes)
+    //         }
+    //
+    //     } catch (err) {
+    //         console.error(err.message);
+    //     }
+    //     return false;
+    // }
 
 
     return (
         <React.Fragment>
             <div className={LoginRegisterStyle.loginRegisterButtons}>
                 <div className={LoginRegisterStyle.loginButton}>
-                    <GoogleLogin clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                                 render={renderProps => (
-                                     <button type={'button'} onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                                     <AiFillGoogleCircle size={27} />
-                                         <span>{google_t}</span>
-                                     </button>
-                                 )}
-                                 buttonText={"Log in with Google"}
-                                 onSuccess={handleLogin}
-                                 onFailure={handleFailure}
-                                 cookiePolicy={"single_host_origin"}>
-                    </GoogleLogin>
+                    {/*<GoogleLogin clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}*/}
+                    {/*             render={renderProps => (*/}
+                    {/*                 <button type={'button'} onClick={renderProps.onClick} disabled={renderProps.disabled}>*/}
+                    {/*                 <AiFillGoogleCircle size={27} />*/}
+                    {/*                     <span>{google_t}</span>*/}
+                    {/*                 </button>*/}
+                    {/*             )}*/}
+                    {/*             buttonText={"Log in with Google"}*/}
+                    {/*             onSuccess={handleLogin}*/}
+                    {/*             onFailure={handleFailure}*/}
+                    {/*             cookiePolicy={"single_host_origin"}>*/}
+                    {/*</GoogleLogin>*/}
                 </div>
             </div>
 
